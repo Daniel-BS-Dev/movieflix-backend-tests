bfee6dbb43334b6dfc7835cbe4803d63f99bdb4b
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-08-17 14:33:48 -0300

    Add Hook Form

--- front-web/src/pages/Auth/index.tsx
+++ front-web/src/pages/Auth/index.tsx
@@ -1,26 +1,42 @@
 import Button from '../../core/components/Button';
+import { useForm } from 'react-hook-form';
 import './styles.scss';
 
+type FormData = {
+    email: string;
+    password: string;
+}
+
 const Auth = () => {
+     const { register, handleSubmit } = useForm<FormData>();
+
+     const onSubmit = (data: FormData) => {
+         console.log(data);
+     }
+
     return (
         <div className="auth-container">
             <div className="auth-login">
                 LOGIN
             </div>
             <div>
-                <form >
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <input 
                     type="email" 
                     className="form-control auth-form"
                     placeholder="Email"
+                    {...register("email", 
+                    { required: true })}
                     />
                     <input 
                     type="password" 
                     className="form-control auth-form"
                     placeholder="Senha"
+                    {...register("password", 
+                    { required: true })}
                     />
-                </form>
-                <Button text="fazer login"/>
+                    <Button text="fazer login"/>
+                </form>  
             </div>
         </div>
     );
