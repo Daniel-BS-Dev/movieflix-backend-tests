0b3571b18323d881c8c63d551443dfe5afe87884
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-08-20 09:29:38 -0300

    add field review

--- front-web/src/core/components/Review/index.tsx
+++ front-web/src/core/components/Review/index.tsx
@@ -1,12 +1,28 @@
+import { useForm } from 'react-hook-form';
 import Button from '../Button';
 import './styles.scss';
 
+type FormData = {
+    review: string;
+}
+
 const Review = () => {
+    const { register, handleSubmit } = useForm<FormData>();
+
+    const onSubmit = (data: FormData) => {
+        console.log(data);
+    }
+
     return(
        <div className="review-container">
-           <form className="review-content">
-               <input type="text" />
-               <Button text="Salvar avaliação"/>
+           <form onSubmit={handleSubmit(onSubmit)} className="review-content" >
+               <input 
+                 type="text" 
+                 placeholder="Deixe sua avalição aqui!"
+                 {...register("review", 
+                 { required: true })}
+                />
+                <Button text="SALVAR AVALIAÇÃO" />
            </form>
            
        </div>
