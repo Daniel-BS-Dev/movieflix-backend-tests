765194f53efe4efed57657962899422e9b02ccc9
Daniel-BS-Dev <danielbenedito355@gmail.com>
2022-01-25 16:34:58 -0300

    movie list genre

--- frontend/src/pages/Home/Movie/index.tsx
+++ frontend/src/pages/Home/Movie/index.tsx
@@ -1,6 +1,25 @@
+import './styles.css';
+
 const Movie = () => {
+    const movie =    {
+        title: "BUFFALO BOYS",
+        subTitle: " ",
+        year: 2018,
+        imgUri: "https://www.themoviedb.org/t/p/original/tLKashUlRCN8zzYBXiago4eJPho.jpg",
+        synopsis: "Traído pelas forças coloniais holandesas, Arana consegue escapar junto com seus sobrinhos Jamar e Suwo, os filhos recém-nascidos de Hamza, um dos últimos sultões indonésios lutando contra a tirania estrangeira. Em 1860, depois de vagar por anos nas planícies do Velho Oeste americano, Arana e os dois irmãos retornam à Indonésia para vingar seus entes queridos mortos e punir o homem mau que causou sua desgraça.",
+        genreId: 1,
+    
+    }
+
     return(
-        <h1>movie</h1>
+       <section className='container-movie'>
+           <div className='content-movie'>
+               <img src={movie?.imgUri}/>
+           </div>
+           <div className='container-name-movie'>
+               <h3>{movie?.title}</h3>
+           </div>
+       </section>
     )
 
 }
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -11,37 +11,79 @@ const Home = () => {
       <div className="navbar-home"><NavBar /></div>
       <div className="slider-home"><SliderMovie /></div> 
       <div className="list-image-home">
-       <div>
+       <div className="catalog-movie-home">
          <h2>Acão</h2>
+         <div className="movies-home">
          <Movie />
+         <Movie />
+         <Movie />
+        
+         </div> 
        </div>
-       <div>
+       <div className="catalog-movie-home">
          <h2>Terror</h2>
+         <div className="movies-home">
          <Movie />
+         <Movie />
+         <Movie />
+         
+         </div> 
+          <div className="movies-home"> 
        </div>
-       <div>
+       <div className="catalog-movie-home">
          <h2>Aventura</h2>
+         <div className="movies-home">
          <Movie />
+         <Movie />
+         <Movie />
+        
+         </div>      
        </div>
-       <div>
+       <div className="catalog-movie-home">
          <h2>Comedia</h2>
+         <div className="movies-home">
          <Movie />
+         <Movie />
+         <Movie />
+        
+         </div>       
        </div>
-       <div>
+       <div className="catalog-movie-home">
          <h2>Romance</h2>
+         <div className="movies-home">
          <Movie />
+         <Movie />
+         <Movie />
+        
+         </div>      
        </div>
-       <div>
+       <div className="catalog-movie-home">
          <h2>Fantasia</h2>
+         <div className="movies-home">
          <Movie />
+         <Movie />
+         <Movie />
+        
+         </div>      
        </div>
-       <div>
+       <div className="catalog-movie-home">
          <h2>Familia</h2>
+         <div className="movies-home">
          <Movie />
+         <Movie />
+         <Movie />
+      
+         </div>       
        </div>
-       <div>
+       <div className="catalog-movie-home">
          <h2>Trama</h2>
+         <div className="movies-home">
          <Movie />
+         <Movie />
+         <Movie />
+        
+         </div> 
+        </div>
        </div>
       </div>
       </section>
